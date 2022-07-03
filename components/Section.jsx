2cdbbe3d3849316180eb22b46ceddd8b2e31b131
Facu <Facu@michelles-mbp-1.home>
2022-07-03 23:42:15 +0200

    animation framer

--- components/Section.jsx
+++ components/Section.jsx
@@ -1,14 +1,20 @@
+import { motion } from "framer-motion"
+import { useRef } from "react";
+
+
 import style from "../styles/Section.module.css";
 
 import Image from "next/image";
 
 const Section = () => {
+    const constraintsRef = useRef(null);
+
   return (
-    <section className={style.display}>
+    <section className={style.display} ref={constraintsRef}>
       <div className={style.textContainer}>
         <h1 className={style.title}>Live </h1><h1 className={style.titleColor}>Design.</h1>
       </div>
-      <div className={style.imgContainer}/>
+      <motion.div className={style.imgContainer} drag dragConstraints={constraintsRef}/>
     </section>
   );
 };
